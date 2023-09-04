a0afeae7da3cd8cec11cf2b34e2e47cc9523bf64
Barelella <ellabarel1@gmail.com>
2023-09-04 09:08:35 +0300

    Update colab notebook Allow train_model to import utils Fixed shuffle_meld script

--- utils/shuffle_meld.py
+++ utils/shuffle_meld.py
@@ -5,8 +5,8 @@ label_conveter = {'neutral': 0, 'anger': 1, 'joy': 2, 'sadness': 3, 'surprise': 
 
 def sample_df(df, num_samples):
     for label in label_conveter.keys():
-        df_label = df[df.label == label]
-        df_sampled = df_label.sample(n=num_samples//7)
+        df_sampled = df[df.label == label]
+        df_sampled = df_sampled.sample(n=num_samples//7, replace=True)
         if label_conveter[label] == 0:
             output_df = df_sampled
         else:
@@ -24,33 +24,34 @@ def sample_data_set(meta_csv_file, num_train_samples, num_test_samples, num_dev_
     df = pd.read_csv(meta_csv_file)
     
     src_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/train/wav/"
-    dst_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/train/wav_800/"
-    # os.mkdir(dst_path) 
+    dst_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/train/wav_for_cloud/"
+    os.mkdir(dst_path) 
     df_train = df[df.state == "train"]
     sampled_train_df = sample_df(df_train, num_train_samples)
     copy_wav_files_in_df(sampled_train_df, src_path, dst_path)
 
     src_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/test/wav/"
-    dst_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/test/wav_800/"
+    dst_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/test/wav_for_cloud/"
     os.mkdir(dst_path)
     df_test = df[df.state == "test"]
     sampled_test_df = sample_df(df_test, num_test_samples)
     copy_wav_files_in_df(sampled_test_df, src_path, dst_path)
 
     src_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/dev/wav/"
-    dst_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/dev/wav_800/"
+    dst_path = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/MELD.Raw/dev/wav_for_cloud/"
     os.mkdir(dst_path)
     df_dev = df[df.state == "dev"]
     sampled_dev_df = sample_df(df_dev, num_dev_samples)
     copy_wav_files_in_df(sampled_dev_df, src_path, dst_path)
 
     output_df = pd.concat([sampled_train_df, sampled_test_df, sampled_dev_df])
-    output_df.to_csv("C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/S4SpeechFormer/metadata/metadata_meld_800.csv")
+    output_df.reset_index(drop=True)
+    output_df.to_csv("C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/S4SpeechFormer/metadata/metadata_meld_for_cloud.csv")
 
 
 meta_csv_file = "C:/Users/Ella bar-El/Documents/Deep_Learning/SpeechFormerProject/S4SpeechFormer/metadata/metadata_meld copy.csv"
-num_train_samples = 800
-num_test_samples = 100
-num_dev_samples = 50
+num_train_samples = 2000
+num_test_samples = 500
+num_dev_samples = 200
 
 sample_data_set(meta_csv_file, num_train_samples, num_test_samples, num_dev_samples)
